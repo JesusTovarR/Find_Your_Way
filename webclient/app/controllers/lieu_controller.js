869853cbb55fdfbfdc04fb3807824f039447f986
JesusTovarR <jesustovar678@gmail.com>
2017-02-11 04:35:59 +0100

    countdown

--- webclient/app/controllers/lieu_controller.js
+++ webclient/app/controllers/lieu_controller.js
@@ -1,6 +1,6 @@
 
-angular.module('app').controller('LieuController', ['$scope', '$rootScope', '$http', 'Lieu', 'LieuFactory',
-    function($scope, $rootScope, $http, Lieu, LieuFactory){
+angular.module('app').controller('LieuController', ['$scope', '$rootScope','$timeout', '$http', 'LieuFactory',
+    function($scope, $rootScope, $timeout, $http, LieuFactory){
         $rootScope.in1=true;
         $rootScope.in2=true;
         $rootScope.in3=true;
@@ -11,6 +11,23 @@ angular.module('app').controller('LieuController', ['$scope', '$rootScope', '$ht
         $rootScope.destFinal3=true;
         $rootScope.destFinal4=true;
         $rootScope.destFinal5=true;
+
+        $scope.regle=false;
+        $scope.regle1=false;
+        $scope.regle2=true;
+        $scope.regle3=true;
+        $scope.regle4=true;
+        $scope.regle5=true;
+        $scope.regle6=true;
+        $scope.disNext=false;
+        $scope.disBack=true;
+        $scope.reglesIndications=true;
+        $scope.indicationsRegles=true;
+        $scope.gameOver=true;
+
+        $scope.counter = 90;
+        var stopped;
+
 
         $scope.newGame = function () {
 
@@ -24,10 +41,32 @@ angular.module('app').controller('LieuController', ['$scope', '$rootScope', '$ht
                 $scope.destinationFinal();
                 $scope.lieux();
                 $rootScope.in1=false;
+                $scope.regle=true;
+                $scope.reglesIndications=true;
+                $scope.indicationsRegles=false;
+                $scope.countdown();
             }, function (error) {
                 console.log('error');
             });
         };
+
+        $scope.countdown = function() {
+            stopped = $timeout(function() {
+                console.log($scope.counter);
+                $scope.counter--;
+                if($scope.counter===0){
+                    $scope.stop();
+                    $scope.gameOver==false;
+                }else{
+                    $scope.countdown();
+                }
+            }, 1000);
+        };
+
+        $scope.stop = function(){
+            $timeout.cancel(stopped);
+
+        }
 
         $scope.indications = function (){
             LieuFactory.indications($scope.partie.id, $scope.partie.token).then(function (response){
@@ -95,6 +134,55 @@ angular.module('app').controller('LieuController', ['$scope', '$rootScope', '$ht
             },function (error) {
                 console.log('error');
             });
+        };
+
+        $scope.regles = function (){
+            if($scope.reglesIndications===true){
+                $scope.reglesIndications=false;
+            }
+        };
+
+
+        $scope.next = function (){
+            if($scope.regle1===false){
+                $scope.regle1=true;
+                $scope.regle2=false;
+                $scope.disBack=false;
+            }else if($scope.regle2===false){
+                $scope.regle2=true;
+                $scope.regle3=false;
+            }else if($scope.regle3===false){
+                $scope.regle3=true;
+                $scope.regle4=false;
+            }else if($scope.regle4===false){
+                $scope.regle4=true;
+                $scope.regle5=false;
+            }else if($scope.regle5===false){
+                $scope.regle5=true;
+                $scope.regle6=false;
+                $scope.disNext=true;
+            }
+        };
+
+        $scope.back = function (){
+            if($scope.regle2===false){
+                $scope.regle2=true;
+                $scope.regle1=false;
+                $scope.disBack=true;
+            }else if($scope.regle3===false){
+                $scope.regle3=true;
+                $scope.regle2=false;
+            }else if($scope.regle4===false){
+                $scope.regle4=true;
+                $scope.regle3=false;
+            }else if($scope.regle5===false){
+                $scope.regle5=true;
+                $scope.regle4=false;
+            }else if($scope.regle6===false){
+                $scope.regle6=true;
+                $scope.regle5=false;
+                $scope.disNext=false;
+            }
         };
     }
 ]);
